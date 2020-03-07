807965c96d34f506b5c570ba36f3edbb6ff0350b
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-03-07 14:34:53 +0000

    ADD toJson and fromJson methods

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -5,4 +5,16 @@ import '../../domain/entities/number_trivia.dart';
 class NumberTriviaModel extends NumberTrivia {
   NumberTriviaModel({@required String text, @required int number})
       : super(text: text, number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+        text: json['text'] as String, number: (json['number'] as num).toInt());
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
 }
