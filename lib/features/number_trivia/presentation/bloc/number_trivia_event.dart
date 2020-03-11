f1c319b83bc9c1bcf3a025a32ea0c32832c4d3a7
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-03-11 16:22:53 +0000

    ADD GetTrivia events in blocEvents

--- lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -1,5 +1,13 @@
 import 'package:equatable/equatable.dart';
 
 abstract class NumberTriviaEvent extends Equatable {
-  NumberTriviaEvent();
+  NumberTriviaEvent([List props = const <dynamic>[]]) : super(props);
 }
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  final String numberString;
+
+  GetTriviaForConcreteNumber(this.numberString) : super([numberString]);
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {}
