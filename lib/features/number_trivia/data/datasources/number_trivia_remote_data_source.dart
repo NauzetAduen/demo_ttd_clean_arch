8bb4fecdd2c20367f7960e63a27c841efa3df324
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-03-11 13:10:54 +0000

    ADD getRandom and Concrete NTrivia

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+import 'package:demo_tdd_clean_arch/core/error/exceptions.dart';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
+
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaRemoteDataSource {
@@ -8,4 +14,32 @@ abstract class NumberTriviaRemoteDataSource {
   ///http://numbersapi.com/random
   ///Throws [ServerException]
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) =>
+      _getTriviaFromUrl('$number');
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() =>
+      _getTriviaFromUrl('random');
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String endpoint) async {
+    const String base = 'http://numbersapi.com/';
+    final response = await client.get(
+      base + endpoint,
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(
+          json.decode(response.body) as Map<String, dynamic>);
+    } else {
+      throw ServerException();
+    }
+  }
 }
