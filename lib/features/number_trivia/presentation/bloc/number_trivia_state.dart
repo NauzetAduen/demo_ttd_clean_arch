593f2dc8f74ccda361491ba78b3c7bbba4d49502
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-03-11 16:23:11 +0000

    ADD new bloc States and refactor empty

--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,10 +1,24 @@
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../../domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaState extends Equatable {
-  NumberTriviaState();
+  NumberTriviaState([List props = const <dynamic>[]]) : super(props);
 }
 
-class InitialNumberTriviaState extends NumberTriviaState {
-  @override
-  List<Object> get props => [];
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({@required this.trivia}) : super([trivia]);
+}
+
+class Error extends NumberTriviaState {
+  final String errorMessage;
+
+  Error({@required this.errorMessage}) : super([errorMessage]);
 }
