e01d53b7f5b6ef471bb683a8f0f14fc81f96603e
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-03-12 12:15:56 +0000

    ADD fields to NTBloc

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,8 +1,20 @@
 import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+
+import '../../../../core/util/input_converter.dart';
+import '../../domain/usecases/get_concrete_number_trivia.dart';
+import '../../domain/usecases/get_random_number_trivia.dart';
 import './bloc.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc(this.getConcreteNumberTrivia, this.getRandomNumberTrivia,
+      this.inputConverter);
+
   @override
   NumberTriviaState get initialState => Empty();
 
