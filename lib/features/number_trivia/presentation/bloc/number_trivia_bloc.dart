5f04b036694695a1a90187a04ced048a9b243e07
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-03-12 12:44:41 +0000

    ADD Error flow to GetriviaConcrete in mapToEvent

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -7,6 +7,11 @@ import '../../domain/usecases/get_concrete_number_trivia.dart';
 import '../../domain/usecases/get_random_number_trivia.dart';
 import './bloc.dart';
 
+const String serverFailureMessage = "Server Failure";
+const String cacheFailureMessage = "Cache Failure";
+const String invalidInputFailureMessage =
+    "Invalid input\nThe number must be a natural number";
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   final GetConcreteNumberTrivia getConcreteNumberTrivia;
   final GetRandomNumberTrivia getRandomNumberTrivia;
@@ -22,6 +27,16 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   Stream<NumberTriviaState> mapEventToState(
     NumberTriviaEvent event,
   ) async* {
-    // TODO: Add Logic
+    if (event is GetTriviaForConcreteNumber) {
+      //
+      final inputEither =
+          inputConverter.stringToUnsignedInteger(event.numberString);
+      yield* inputEither.fold(
+        (failure) async* {
+          yield Error(errorMessage: invalidInputFailureMessage);
+        },
+        (integer) => throw UnimplementedError(),
+      );
+    }
   }
 }
